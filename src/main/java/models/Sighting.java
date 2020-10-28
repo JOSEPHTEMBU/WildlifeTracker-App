896dc5b69fd61a7341236883981eb87293603212
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-28 16:29:23 +0300

    updated sighting class

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,47 @@
 package models;
 
 public class Sighting {
+
+    private int id;
+    private Animal animal;
+    private int rangerId;
+    private int locationId;
+
+    public Sighting(Animal animal, int rangerId, int locationId) {
+        this.animal = animal;
+        this.rangerId = rangerId;
+        this.locationId = locationId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public int getRangerId() {
+        return rangerId;
+    }
+
+    public void setRangerId(int rangerId) {
+        this.rangerId = rangerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
 }
