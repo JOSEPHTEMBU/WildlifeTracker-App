28150c891bbdac8f2c38cae0406d15c06334fe3b
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-28 16:14:58 +0300

    updated endangered animal class

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,31 @@
 package models;
 
-public class EndangeredAnimal {
+public class EndangeredAnimal extends Animal{
+
+    private String health;
+    private int age;
+
+
+    public EndangeredAnimal(int id, String name, String health, int age) {
+        super(id, name);
+        this.health = health;
+        this.age = age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
