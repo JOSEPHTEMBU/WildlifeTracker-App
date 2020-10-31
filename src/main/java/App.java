780f3449660bde8dc036079d4a4ff2cef1428fc0
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-31 09:16:38 +0300

    update App class to automatically pick heroku port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,7 +27,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        getHerokuAssignedPort();
+        port(getHerokuAssignedPort());
 
         staticFileLocation("/public");
 
